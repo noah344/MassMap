951b0b16c80d265ff27f6c31e8d9d7b79660b101
NargenPargen <noah81298@gmail.com>
2020-04-22 19:21:32 -0400

    Fixed issue in setup file.

--- setup.py
+++ setup.py
@@ -1,13 +1,36 @@
 #!/usr/bin/env python3
 
 from subprocess import run, DEVNULL
-from main import setup_logging
 import logging
 
+class bcolors:
+    """Used to provide color to logging output."""
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 to_install = ["qdarkstyle", "pyqt5", "tqdm", "python-libnmap", "selenium"]
-setup_logging()
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s', 
+    level=logging.INFO,
+    handlers=[
+        logging.StreamHandler()
+    ])
+
 logging.getLogger().setLevel(logging.DEBUG)
-logging.info("Beggining Installation of Dependencies")
+
+logging.addLevelName(logging.WARNING, bcolors.WARNING + bcolors.BOLD + logging.getLevelName(logging.WARNING) + bcolors.ENDC)
+logging.addLevelName(logging.CRITICAL, bcolors.FAIL + bcolors.BOLD + bcolors.UNDERLINE + logging.getLevelName(logging.CRITICAL) + bcolors.ENDC)
+logging.addLevelName(logging.INFO, bcolors.BOLD + bcolors.OKGREEN + logging.getLevelName(logging.INFO) + bcolors.ENDC)
+logging.addLevelName(logging.DEBUG, bcolors.OKBLUE + logging.getLevelName(logging.DEBUG) + bcolors.ENDC)
+
+logging.info("Beginning Installation of Dependencies")
 for i in to_install:
     try:
         logging.debug("Attempting to install %s." % i)
